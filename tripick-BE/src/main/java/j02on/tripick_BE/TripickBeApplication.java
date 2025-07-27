5f81cc5161e0a192f6cb1ee55c04d746efb1a628
j02on <pjylove08@naver.com>
2025-07-27 15:50:16 +0900

    feat :: setting

---
+++ tripick-BE/src/main/java/j02on/tripick_BE/TripickBeApplication.java
@@ -0,0 +1,13 @@
+package j02on.tripick_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripickBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripickBeApplication.class, args);
+	}
+
+}
