de716ffd54d7c882ba21a0b2390180a7cca28a5d
j02on <pjylove08@naver.com>
2025-08-03 15:18:47 +0900

    fix : error fix

--- models/Logs.js
+++ models/Logs.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const logSchema = new mongoose.Schema(
   {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     planId: {
       type: Number,
       ref: 'Plan',
