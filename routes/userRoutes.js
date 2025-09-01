f2f668d4029056088326e2e6036daf87c5fc9819
j02on <pjylove08@naver.com>
2025-09-01 16:05:21 +0900

    feat : 다른 사용자 조회 api

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const verifyToken = require('../middleware/authHandler');
-const { getMyProfile, delUser } = require('../controllers/userController');
+const { getMyProfile, delUser, userProfileGet } = require('../controllers/userController');
 
 router.get('/', verifyToken, getMyProfile);
 router.delete('/', verifyToken, delUser);
+router.get('/:userId', verifyToken, userProfileGet)
 
 module.exports = router;
